dfb0e4d7f105dbb9850cc74a8ed4bbb8bb49dcbb
RedOuaneAO <redouaneao97@gmail.com>
2023-10-21 16:41:50 +0100

    logout

--- src/main/java/com/example/resourciumoptima2/servlet/LogoutServlet.java
+++ src/main/java/com/example/resourciumoptima2/servlet/LogoutServlet.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession session=request.getSession();
+        session.invalidate();
+        response.sendRedirect("login.jsp");
     }
 
     @Override
